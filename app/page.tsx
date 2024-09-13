dd34eef8eca8e1374b69bc14ba5d0f935e9d989f
Leonardo Ferreira <leonardoferreira-dev@outlook.com>
2024-09-13 20:49:37 -0300

    fix fotos

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable */
+
 "use client";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 import { useState, FormEvent, useEffect } from "react";
 import Image from "next/image";
-import imageBezerra from "@/images/bezerra.png";
 import InputMask from "react-input-mask";
 import { format } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
+
+import Slider from "react-slick";
 
 interface Bid {
   name: string;
@@ -19,7 +24,7 @@ function AuctionPage() {
   const [contact, setContact] = useState<string>("");
   const [amount, setAmount] = useState<string>("");
   const [latestBid, setLatestBid] = useState<Bid | null>(null);
-  const [highestBid, setHighestBid] = useState<Bid | null>(null);
+  const [highestBid, setHighestBid] = useState<Bid | null>({});
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
 
   const auctionEndDate = new Date("2024-09-30T23:59:59");
@@ -45,9 +50,13 @@ function AuctionPage() {
       highestBid &&
       parseFloat(amount.replace(",", ".")) <= parseFloat(highestBid.amount)
     )
-      setErrors(newErrors);
+      newErrors.amount = `O lance deve ser maior que R$ ${toPrice(
+        highestBid.amount
+      )}`;
 
-    if (Object.keys(newErrors).length > 0) return; // Se houver erros, não envie o formulário
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length > 0) return;
 
     const formattedAmount = amount.replace(",", ".");
 
@@ -58,9 +67,6 @@ function AuctionPage() {
       timestamp: new Date(),
     };
 
-    console.log("newBid", newBid);
-
-    // Enviar dados para o servidor
     await fetch("/api/bids", {
       method: "POST",
       headers: {
@@ -69,7 +75,6 @@ function AuctionPage() {
       body: JSON.stringify(newBid),
     });
 
-    // Atualizar as ofertas após o envio
     await fetchBids();
     setName("");
     setContact("");
@@ -84,7 +89,6 @@ function AuctionPage() {
         throw new Error("Failed to fetch bids");
       }
       const data = await response.json();
-      console.log("trazendo data", data);
       setLatestBid(data.latestBid);
       setHighestBid(data.highestBid);
     } catch (error) {
@@ -98,20 +102,63 @@ function AuctionPage() {
 
   const isAuctionOver = new Date() > auctionEndDate;
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true, // Ajusta a altura do slider com base no slide atual
+  };
+
   return (
     <div
-      className="flex flex-col items-center p-8 min-h-screen "
+      className="flex flex-col items-center p-8 min-h-screen"
       style={{ background: "#F2F7FA" }}
     >
       <h1 className="text-4xl text-black font-bold mb-8">Leilão de Bezerra</h1>
 
-      <Image
-        src={imageBezerra}
-        alt="Bezerra"
-        width={400}
-        height={300}
-        priority
-      />
+      {/* Carrossel de imagens e vídeo */}
+      <div className="w-full max-w-lg mb-8 min-h-[300px]">
+        <Slider {...sliderSettings}>
+          <div>
+            <Image
+              src="https://leonardoferreiradev.s3.sa-east-1.amazonaws.com/bezerro1.jpeg"
+              alt="Bezerro 1"
+              width={500} // Ajusta largura
+              height={350} // Ajusta altura
+              layout="responsive"
+            />
+          </div>
+          <div>
+            <Image
+              src="https://leonardoferreiradev.s3.sa-east-1.amazonaws.com/bezerro2.jpeg"
+              alt="Bezerro 2"
+              width={500}
+              height={350}
+              layout="responsive"
+            />
+          </div>
+          <div>
+            <Image
+              src="https://leonardoferreiradev.s3.sa-east-1.amazonaws.com/bezerro3.jpeg"
+              alt="Bezerro 3"
+              width={500}
+              height={350}
+              layout="responsive"
+            />
+          </div>
+          <div>
+            <video controls width="500" height="300" className="w-full h-full">
+              <source
+                src="https://leonardoferreiradev.s3.sa-east-1.amazonaws.com/bezerra.mp4"
+                type="video/mp4"
+              />
+              Seu navegador não suporta o elemento de vídeo.
+            </video>
+          </div>
+        </Slider>
+      </div>
 
       <p className="max-w-sm text-lg mt-4 text-center text-black">
         Participe do leilão da bezerra. Faça seu lance antes que o leilão
@@ -140,7 +187,7 @@ function AuctionPage() {
                     errors.name ? "border-red-500" : ""
                   }`}
                   required
-                  maxLength={40} // Adiciona o limite de caracteres
+                  maxLength={40}
                 />
                 {errors.name && <p className="text-red-500">{errors.name}</p>}
               </div>
@@ -151,17 +198,19 @@ function AuctionPage() {
                   value={contact}
                   onChange={(e) => setContact(e.target.value)}
                 >
-                  {(inputProps: any) => (
-                    <input
-                      {...inputProps}
-                      type="text"
-                      placeholder=""
-                      className={`border p-2 mb-4 text-black  bg-slate-200 ${
-                        errors.contact ? "border-red-500" : ""
-                      }`}
-                      required
-                    />
-                  )}
+                  {(inputProps: any) => {
+                    return (
+                      <input
+                        {...inputProps}
+                        type="text"
+                        placeholder=""
+                        className={`border p-2 mb-4 text-black  bg-slate-200 ${
+                          errors.contact ? "border-red-500" : ""
+                        }`}
+                        required
+                      />
+                    );
+                  }}
                 </InputMask>
                 {errors.contact && (
                   <p className="text-red-500">{errors.contact}</p>
@@ -174,65 +223,69 @@ function AuctionPage() {
                   value={amount}
                   onChange={(e) => setAmount(e.target.value)}
                 >
-                  {(inputProps: any) => (
-                    <input
-                      {...inputProps}
-                      type="text"
-                      placeholder=""
-                      className={`border p-2 mb-4 bg-slate-200 text-black ${
-                        errors.amount ? "border-red-500" : ""
-                      }`}
-                      dir="rtl" // Adiciona a propriedade para escrita da direita para a esquerda
-                      required
-                    />
-                  )}
+                  {(inputProps: any) => {
+                    return (
+                      <input
+                        {...inputProps}
+                        type="text"
+                        placeholder=""
+                        className={`border p-2 mb-4 bg-slate-200 text-black ${
+                          errors.amount ? "border-red-500" : ""
+                        }`}
+                        dir="rtl"
+                        required
+                      />
+                    );
+                  }}
                 </InputMask>
 
                 <p className="text-red-500 mb-4">
-                  {" "}
-                  O lance minimo é de R$ {toPrice(highestBid?.amount) || "0,00"}
+                  O lance mínimo é de R$ {toPrice(highestBid?.amount) || "0,00"}
                 </p>
               </div>
               <button
                 type="submit"
-                className="bg-green-500 text-white px-4 h-11 rounded-md"
+                className="bg-green-500 text-white px-4 h-12 flex items-center justify-center rounded-lg"
               >
                 Enviar Lance
               </button>
-              <p className="mt-2 text-red-500">
-                Data limite:{" "}
-                {format(auctionEndDate, "dd/MM/yyyy", { locale: ptBR })}
-              </p>
             </div>
           </form>
         ) : (
-          <p className="text-red-500 font-bold mt-6">Leilão encerrado!</p>
+          <div className="p-4 text-center">
+            <p className="text-lg font-bold">O leilão terminou!</p>
+          </div>
         )}
       </div>
-      <div className="flex flex-col bg-white rounded-2xl shadow-sm mt-8 w-full max-w-sm">
-        <div
-          className="w-full h-12 justify-center flex items-center rounded-t-2xl shadow-sm"
-          style={{ background: "#00B4B0" }}
-        >
-          <span className="text-base"> Último Lance</span>
-        </div>
-        <div className="p-2 w-full flex overflow-hidden">
-          <ul className="flex max-w-lg list-disc list-inside text-black overflow-wrap-break-word">
-            {latestBid ? (
-              <li className="border-b py-2">
-                {latestBid.name} <b>R${toPrice(latestBid.amount)}</b> -{" "}
-                {format(new Date(latestBid.timestamp), "dd/MM/yyyy", {
+
+      {latestBid && (
+        <div className="bg-white rounded-2xl shadow-sm mt-8 max-w-sm w-full">
+          <div
+            className="w-full h-12 justify-center flex items-center rounded-t-2xl shadow-sm"
+            style={{ background: "#00B4B0" }}
+          >
+            <span className="text-base">Último lance</span>
+          </div>
+          <div className="p-4">
+            <p className="text-gray-800">
+              <strong>Nome:</strong> {latestBid.name}
+            </p>
+            <p className="text-gray-800">
+              <strong>Valor:</strong> R$ {toPrice(latestBid.amount)}
+            </p>
+            <p className="text-gray-800">
+              <strong>Data:</strong>{" "}
+              {format(
+                new Date(latestBid.timestamp),
+                "dd 'de' MMMM 'às' HH:mm",
+                {
                   locale: ptBR,
-                })}
-              </li>
-            ) : (
-              <li className="text-gray-500 text-black">
-                Nenhum lance até agora.
-              </li>
-            )}
-          </ul>
+                }
+              )}
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
